4d40f58e2478afbeba471445d657e2b71d347860
Cthul <dylan.devaure@outlook.fr>
2019-06-13 10:39:29 +0200

    heure locale et React

---
+++ src/Clock.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class Clock extends React.Component{
+    // Première fonction qui execute pour créer le premier composant --> C'est celle qui le construit
+    constructor(props){
+        super(props)
+        this.state={
+            date: new Date()
+        };
+    }
+
+    // La fonction qui affiche l'HTML sur l'écran  / render = afficher
+    render(){
+        return (
+        <div>
+            <h1>heure locale</h1>
+            <h2>Il est actuellement {new Date().toLocaleTimeString()}</h2>
+        </div>
+        )
+
+    }
+
+    // Une fois que l'affichage a lieu, la fonction suivante s'execute directement.
+    // COmponentDiDMount = le composant vient de se mettre en place
+    componentDidMount(){
+        this.timerID = setInterval(()=>this.tictac(), 1000);
+
+    }
+
+    // Voici une fonction qui permet de mettre à jour la date dans l'état (this.state.date)
+    // ATT : qui dit mise à jour de l'état dit : setState
+    tictac(){
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    // Juste avant de finir un lifecycle et de quitter le composant, nous effaçons le contenu de l'intervalle de temps
+
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+
+
+
+}
+
+
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import './index.css';
 import App from './App';
 import Voiture from "./Voiture";
 import Garage from "./Garage";
+import Tick from './Tick';
 import Square from "./Square";
 import Board from "./Board";
 import Game from "./Game";
+import Clock from "./Clock";
 import * as serviceWorker from './serviceWorker';
 
-// function monComposant(){
-//     return <p>Trop cool ce nouveau composant</p>;
+// function Tick2(){
+
+//     const element= (
+//     <div>
+//         <h1>heure locale</h1>
+//         <h2>Il est actuellement {new Date().toLocaleTimeString()}</h2>
+//     </div>
+//     );
+
+//     ReactDOM.render(element,document.getElementById('popo2') );
+
 // }
+
+// L'appelle à la fonction Tick2 chaque seconde
+// setInterval(Tick2, 1000);
 
 // Syntaxe JSX - Javascript XML, elle nous permet d'écrire de l'HTML en React.
 // const myelement = (<p>Coucou voici mon html {5+5}</p>);
@@ -25,6 +39,12 @@ import * as serviceWorker from './serviceWorker';
 // ReactDOM.render(<Garage color="red"/>, document.getElementById('popo') );
 
 ReactDOM.render(<Game />, document.getElementById('root') );
+ReactDOM.render(<Clock />, document.getElementById('popo') );
+
+// Ci dessous une manière d'appeler un composant à partir d'une fonction
+// ReactDOM.render(<Tick />, document.getElementById('popo') );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
